dbdb725fa37025108399d99eda64b539f43dc5bb
Moamen Mohamed <125478677+Moamenmohamed00@users.noreply.github.com>
2025-10-30 14:34:19 +0300

    Implement multi-threading with Runnable tasks

--- Task 1  /Lab_Section3.java
+++ Task 1  /Lab_Section3.java
@@ -1,1 +1,48 @@
+import java.util.ArrayList;
+import java.util.List;
+public class Lab_Section3 {
+    public static void main(String[] args) {
+    List<Runnable> runnables = new ArrayList<>();
+runnables.add(() -> {
+    Thread t1=new Thread("Thread-1");
+    try {//sleep t1
+        Thread.sleep(1000);
+    } catch (InterruptedException e) {
+        e.printStackTrace();
+    }
+    System.out.println(t1.getName() + " is running");
+});
+runnables.add(() -> {
+    Thread t2=new Thread("Thread-2");
+    System.out.println(t2.getName() + " is running");});
+     try {//that sleep main thread
+        Thread.sleep(1000);
+    } catch (InterruptedException e) {
+        e.printStackTrace();
+    }
+runnables.add(() -> {
+    Thread t3=new Thread("Thread-3");
+    try {//sleep t3
+       Thread.sleep(1000);
+   } catch (InterruptedException e) {
+       e.printStackTrace();
+   }
+    System.out.println(t3.getName() + " is running");
+});
+MultiExecutorthat multiExecutor = new MultiExecutorthat(runnables);
+multiExecutor.executeAll();
+ }
+}
+ class MultiExecutorthat extends Thread{
+ private List<Runnable> tasks;
+  MultiExecutorthat(List<Runnable> tasks){
+    this.tasks=tasks;
+  }
+  public void executeAll(){
+    for(Runnable r:tasks){
+      Thread t=new Thread(r);
+      t.start();
+    }
+  }
+}
 
