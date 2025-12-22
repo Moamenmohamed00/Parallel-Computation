6895488f675e88830dbd778d1872d02d83a4da36
KareemEzzat <kareemezzat1222@gmail.com>
2025-12-22 09:27:38 +0200

    Remove unused files and classes from the project

--- project/ss/src/Chunk.java
+++
@@ -1,9 +0,0 @@
-public class Chunk {
-    public final long start;
-    public final long end;
-
-    public Chunk(long start, long end) {
-        this.start = start;
-        this.end = end;
-    }
-}
